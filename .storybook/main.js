1f162af74a9e64953fd1904d24efcabd39f84ae9
rangelovvjobs <rangelovvjobs@gmail.com>
2024-05-22 15:02:18 +0300

    Added Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
