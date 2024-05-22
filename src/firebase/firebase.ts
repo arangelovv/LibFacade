48bd8d61eb17d6079b50ee097ecdb93f056fd91e
rangelovvjobs <rangelovvjobs@gmail.com>
2024-05-22 15:03:09 +0300

    Added Storybook #2

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { firebaseConfig } from "../../../enviorments/enviroment.dev";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestore = getFirestore();
+
+connectAuthEmulator(auth, "http://127.0.0.1:9099");
+connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
