38dfc000f89139691a8c5dc66d83875ba852a5cd
rangelovvjobs <rangelovvjobs@gmail.com>
2024-05-25 15:41:33 +0300

    Created a storybook story for TabsOrganism

--- src/ui/models/tabs.config.ts
+++ src/ui/models/tabs.config.ts
@@ -3,10 +3,9 @@ import { ReactNode } from "react";
 export interface SingleTabConfig {
   label: string;
   index: string;
-  herf: string;
   icon?: any;
   iconPosition?: "start" | "end" | "bottom";
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 export interface TabsOrganismConfig {
