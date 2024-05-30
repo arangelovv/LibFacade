fdf898979ccd214dcd3c0c1b3f4a62a3dce51a9d
rangelovvjobs <rangelovvjobs@gmail.com>
2024-05-30 17:54:04 +0300

    Fixed ButtonAtom bug

--- src/ui/atoms/ButtonAtom.tsx
+++ src/ui/atoms/ButtonAtom.tsx
@@ -48,7 +48,7 @@ export default function ButtonAtom({
 
   return (
     <Button
-      onChangeCapture={onClick}
+      onClick={onClick}
       variant={variant}
       startIcon={buttonIcon}
       sx={{
